4fa7ae8b6c502e507bf8763bfe592588f1e0e828
yogendra-r <yogendra.r@cisinlabs.com>
2024-04-24 19:31:28 +0530

    fecha

--- controllers/admin.js
+++ controllers/admin.js
@@ -1484,14 +1484,14 @@ async function leaderSignup(req, res) {
                 // console.log(result)
             }
             const resl = await sequelize.query(`update usuarios_usuario set primer_nombre= :primer_nombre,primer_apellido= :primer_apellido, segundo_nombre = :segundo_nombre,
-        segundo_apellido = :segundo_apellido,nombre_completo = :nombre_completo, direccion = :direccion, email = :email, celular = :celular,telefono = :telefono, profesion_id = :profesion_id,
-        estado_id = :estado_id, area_id =:area_id, cabildo_id = :cabildo_id ,  distrito_sgip_id = :distrito_sgip_id, grupo_id = :grupo_id,fecha_nacimiento = :fecha_nacimiento,
-        division_id = :division_id,nivel_budista_id = :nivel_budista_id, responsable_gohonzon= :responsable_gohonzon,cargo_responsable_id = :cargo_responsable_id, edicion = :edicion,edited_by  = :edited_by
-        where id = ${req.body.user_id}`, {
-                replacements: {
-                    ...data
-                }
-            })
+            segundo_apellido = :segundo_apellido,nombre_completo = :nombre_completo, direccion = :direccion, email = :email, celular = :celular,telefono = :telefono, profesion_id = :profesion_id,
+            estado_id = :estado_id, area_id =:area_id, cabildo_id = :cabildo_id ,  distrito_sgip_id = :distrito_sgip_id, grupo_id = :grupo_id,fecha_nacimiento = :fecha_nacimiento,
+            division_id = :division_id,nivel_budista_id = :nivel_budista_id, responsable_gohonzon= :responsable_gohonzon,cargo_responsable_id = :cargo_responsable_id, edicion = :edicion,edited_by  = :edited_by,fecha_ingreso : fecha_ingreso,
+            where id = ${req.body.user_id}`, {
+                    replacements: {
+                        ...data
+                    }
+                })
 
         }
         return res.status(200).send({
