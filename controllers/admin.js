5174ba234b14f0062897f75f65b0c434eccc3409
yogendra-r <yogendra.r@cisinlabs.com>
2024-05-15 19:54:34 +0530

    added timeoffset in userdetails api

--- controllers/admin.js
+++ controllers/admin.js
@@ -1295,7 +1295,12 @@ async function getUserDetails(req, res) {
         inner join usuarios_sexo as us on us.id = user.sexo_id 
         where user.id = ${req.body.id}`
             var result = await sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
+            
             result[0].fechadeingreso = new Date(result[0].fechadeingreso)//(.toString()).slice(0, 10)
+            let mins = result[0].fechadeingreso.getTimezoneOffset() ;
+
+            result[0].fechadeingreso.setMinutes(result[0].fechadeingreso.getMinutes() - mins);
+
             console.log('result.fechadeingreso: ', result[0].fechadeingreso);
             console.log(result)
             var hrgrp = []
